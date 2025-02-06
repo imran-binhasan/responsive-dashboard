00c5360c2a2040a892a5f7a1e546bd58509837f6
imran-binhasan <imranbinhasan.work@gmail.com>
2025-02-06 08:08:50 +0600

    initialize project

---
+++ src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element: <div>Hello</div>
+    }
+])
+
+export default Router
